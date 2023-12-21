0ecebc4279eb224dcc5794efd071dfe0b63f8711
yuya024 <yuyaaanissy@gmail.com>
2023-12-21 16:43:39 +0900

    新規登録画面の実装

---
+++ src/components/pages/EmailConfirm.jsx
@@ -0,0 +1,16 @@
+import { emailLink } from "../../common/urls";
+
+export const EmailConfirm = () => {
+  return (
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <h1 className="my-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+          認証メールを送信しました。
+        </h1>
+        <a href={emailLink} className="text-sm font-semibold text-blue-900">
+          こちらからご確認ください<span aria-hidden="true">&rarr;</span>
+        </a>
+      </div>
+    </main>
+  );
+};
