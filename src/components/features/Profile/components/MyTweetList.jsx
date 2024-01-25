a0c8a38e98b08467dcd8059fb9b63c22dff63750
yuya024 <yuyaaanissy@gmail.com>
2024-01-25 15:46:30 +0900

    ログインユーザー自身のツイートのみ削除可能にする

--- src/components/features/Profile/components/MyTweetList.jsx
+++ src/components/features/Profile/components/MyTweetList.jsx
@@ -1,5 +1,6 @@
 export const MyTweetList = (props) => {
-  const { user, tweets, tweetDateFormat, openTweetDeleteModal } = props;
+  const { user, tweets, session, tweetDateFormat, openTweetDeleteModal } =
+    props;
 
   return (
     <>
@@ -19,18 +20,20 @@ export const MyTweetList = (props) => {
                 <p className="text-gray-600 grow">
                   {tweetDateFormat(tweet.created_at)}
                 </p>
-                <p>
-                  <svg
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                    className="w-5 h-5 flex-none mr-3"
-                    onClick={() => openTweetDeleteModal(tweet.id)}
-                  >
-                    <g>
-                      <path d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
-                    </g>
-                  </svg>
-                </p>
+                {user.id === session.user.id && (
+                  <p>
+                    <svg
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                      className="w-5 h-5 flex-none mr-3"
+                      onClick={() => openTweetDeleteModal(tweet.id)}
+                    >
+                      <g>
+                        <path d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
+                      </g>
+                    </svg>
+                  </p>
+                )}
               </div>
               <div className="px-2 w-3/4">
                 {tweet.content
@@ -48,7 +51,7 @@ export const MyTweetList = (props) => {
                 )}
               </div>
               <div className="grid grid-cols-4">
-                <div className="col-span-1">
+                <div className="col-span-1 flex">
                   <svg
                     viewBox="0 0 24 24"
                     width="20"
@@ -59,6 +62,9 @@ export const MyTweetList = (props) => {
                       <path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z"></path>
                     </g>
                   </svg>
+                  {tweet.comment_count !== 0 && (
+                    <p className="px-1 text-sm">{tweet.comment_count}</p>
+                  )}
                 </div>
                 <div className="col-span-1">
                   <svg
